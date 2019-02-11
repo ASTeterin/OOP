d17d9e8fbd2c6aeb52f603b97e049fe1be3794d1
Alex Teterin <at28813905@gmail.com>
2019-02-11 18:18:37 +0300

    добавлены тесты на отсутствие параметров командной строки

--- 1/ConsoleApplication1/ConsoleApplication1/test.bat
+++ 1/ConsoleApplication1/ConsoleApplication1/test.bat
@@ -2,12 +2,29 @@
 
 SET PROGRAM="%~1"
 SET INPUT_FILE="input.txt"
+SET BAD_INPUT_FILE="bad_input.txt"
 SET OUT="%TEMP%\out.txt"
 echo %OUT%
 echo %INPUT_FILE%
 
-%PROGRAM% %INPUT_FILE% "номер первой строки" > %OUT% || goto ERR
-fc %OUT% 18.txt
+%PROGRAM% "empty.txt" "finish university" > %OUT% || goto ERR
+fc %OUT% empty_out.txt
+
+%PROGRAM% "finish university" > %OUT%
+if NOT ERRORLEVEL 1 goto ERR
+fc %OUT% no_param_out.txt
+
+%PROGRAM% %INPUT_FILE%> %OUT%
+if NOT ERRORLEVEL 1 goto ERR
+fc %OUT% no_param_out.txt
+
+%PROGRAM% %INPUT_FILE% "finish university" > %OUT% || goto ERR
+fc %OUT% 7.txt
+
+%PROGRAM% %INPUT_FILE% "I learned to speak German" > %OUT% || goto ERR
+fc %OUT% 14_31.txt
+
+
 
 
 echo All tests passed
