88feedb11a7eeafec680d04ce268ec1702bc73ba
Alex Teterin <at28813905@gmail.com>
2019-02-14 22:59:52 +0300

    исправлены ошибки в bin2dec

--- 1/ConsoleApplication1/bin2dec/Test.bat
+++ 1/ConsoleApplication1/bin2dec/Test.bat
@@ -6,18 +6,22 @@ SET OUT="%TEMP%\out.txt"
 %PROGRAM% > %OUT%
 if NOT ERRORLEVEL 1 goto ERR
 fc %OUT% no_param_out.txt
+if ERRORLEVEL 1 goto ERR
 
 %PROGRAM%  101010101010101010101110000001111100011011000011110010 > %OUT%
 if NOT ERRORLEVEL 1 goto ERR
 fc %OUT% out_of_range_out.txt
+if ERRORLEVEL 1 goto ERR
 
 %PROGRAM% 12345 > %OUT%
 if NOT ERRORLEVEL 1 goto ERR
 fc %OUT% bad_format_out.txt
+if ERRORLEVEL 1 goto ERR
 
 %PROGRAM% 101 > %OUT%
 if ERRORLEVEL 1 goto ERR
 fc %OUT% 5.txt
+if ERRORLEVEL 1 goto ERR
 
 
 echo All tests passed
