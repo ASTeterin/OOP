7971645e8a9f2b9c1de650b730bf917cc13be202
Alex Teterin <at28813905@gmail.com>
2019-03-21 12:04:37 +0300

    добавлены тесты

--- lab2/mini_dictionary/dictionary.h
+++ lab2/mini_dictionary/dictionary.h
@@ -14,3 +14,4 @@ using namespace std;
 bool ParseCommandLine(int argc, char* argv[], string& fileName);
 void StartTranslation(string fileName, map<string, string> &dictionary);
 bool LoadDictionary(string const& fileName, map<string, string> &dictionary);
+string Translate(map<string, string> const& dictionary, string const& word);
