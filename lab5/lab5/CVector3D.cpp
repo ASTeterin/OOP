25af16c9b2901510d5e4608fa349ecf6315ec7a5
Alex Teterin <at28813905@gmail.com>
2019-05-16 18:16:06 +0300

    CrossProduct

--- lab5/lab5/CVector3D.cpp
+++ lab5/lab5/CVector3D.cpp
@@ -127,3 +127,8 @@ double DotProduct(CVector3D const& v1, CVector3D const& v2)
 {
 	return (v1.x * v2.x + v1.y * v2.y + v1.z * v2.z);
 }
+
+CVector3D CrossProduct(CVector3D const& v1, CVector3D const& v2)
+{
+	return (CVector3D(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x));
+}
--- lab5/lab5/CVector3D.h
+++ lab5/lab5/CVector3D.h
@@ -45,4 +45,5 @@ public:
 
 CVector3D const operator*(double scalar, CVector3D const& vector);
 double DotProduct(CVector3D const& v1, CVector3D const& v2);
+CVector3D CrossProduct(CVector3D const& v1, CVector3D const& v2);
 
