b0eaa22f6f2fb43051555e2db9e0393d9d8bfa49
Alex Teterin <at28813905@gmail.com>
2019-06-04 23:26:03 +0300

    добавлены тесты

--- lab7/FindMax/FindMax.h
+++ lab7/FindMax/FindMax.h
@@ -1,1 +1,22 @@
 #pragma once
+
+template <typename T>
+bool FindMax(std::vector<T> const& arr, T& maxValue)
+{
+	if (arr.empty())
+	{
+		return false;
+	}
+	auto maxElement = arr[0];
+
+	for (auto currentElement : arr)
+	{
+		if (maxElement > currentElement)
+		{
+			maxElement = currentElement;
+		}
+	}
+
+	maxValue = maxElement;
+	return true;
+}
