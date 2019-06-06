7e05e2d940b1e529334feb5e01235b53863d526c
Alex Teterin <at28813905@gmail.com>
2019-06-06 18:01:13 +0300

    CMyStack добавлен конструктор по умолчанию, push, pop, isEmpty

---
+++ lab7/MyStack/MyStack.h
@@ -0,0 +1,54 @@
+#pragma once
+
+template <typename T>
+class CMyStack
+{
+
+	struct Node
+	{
+		T data;
+		Node* next;
+	};
+
+public:
+	CMyStack() = default;
+
+	void Push(T const& data)
+	{
+		try 
+		{
+			node* newNode = new Node;
+			newNode.data = data;
+			newNode.next = nullptr;
+			m_pTop.next = newNode;
+			m_pTop = newNode;
+		}
+		catch
+		{
+			throw bad_alloc("");
+		}	
+	}
+
+	bool IsEmpty() const
+	{
+		return !m_pTop;
+	}
+
+	void Pop()
+	{
+		if (!IsEmpty())
+		{
+			m_pTop = m_pTop.next;	
+		}
+		else
+		{
+			throw logic_error("Stack is empty");
+		}
+		
+	}
+
+	
+
+private:
+	T* m_pTop = nullptr;
+};
