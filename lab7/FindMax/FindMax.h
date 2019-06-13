15c4d6c0adf20044be829215bea328da7e53065c
Alex Teterin <at28813905@gmail.com>
2019-06-13 19:26:00 +0300

    добавлена спецификация функции для const char*

--- lab7/FindMax/FindMax.h
+++ lab7/FindMax/FindMax.h
@@ -20,3 +20,24 @@ bool FindMax(std::vector<T> const& arr, T& maxValue)
 	maxValue = *maxElement;
 	return true;
 }
+
+template <>
+bool FindMax(std::vector<const char*> const& arr, const char* & maxValue)
+{
+	if (arr.empty())
+	{
+		return false;
+	}
+	size_t indexOfMaxElement = 0;
+
+	for (size_t i = 1; i < arr.size(); i++)
+	{
+		if (strcmp(arr[i], arr[indexOfMaxElement]) > 0)
+		{
+			indexOfMaxElement = i;
+		}
+	}
+
+	maxValue = arr[indexOfMaxElement];
+	return true;
+}
